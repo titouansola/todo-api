98c5c66d142161a30b11a12320118f85abac085a
Titouan Sola <titouansola0@gmail.com>
2020-03-21 12:26:33 +0100

    change dev port

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use((req, res) => {
 	res.redirect('/api-docs');
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log('Listening on port', port);
 });
